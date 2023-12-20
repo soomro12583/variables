849415e61b1bcea9d5b6a10a1cab669eda0dbfcf
soomro12583 <nadeemkhanasf909@gmail.com>
2023-12-20 22:07:40 +0500

    prectice

--- es06.js
+++ es06.js
@@ -1,7 +1,9 @@
+alert("this is javascript variable for more please press the OK Button");
+
 var name = "Nadeem Soomro"
 name = "Muhammad Nadeem Soomro"
 
 
 
 let a = "My cast is soomro"
-document.write(name+" "+a);
+document.write(name+"<br><br>"+a);
